f12e85508fddf0abec542a8c6a6477d74112b593
Leandro Castillo Borja <leccbo1995@gmail.com>
2023-01-06 15:14:23 -0500

    Crear ventana de detalle producto

---
+++ app/src/main/java/com/leandro1995/retoandroid/activity/DetailProductActivity.kt
@@ -0,0 +1,13 @@
+package com.leandro1995.retoandroid.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.leandro1995.retoandroid.R
+
+class DetailProductActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_product)
+    }
+}
