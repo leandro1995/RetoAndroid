99052387a8aec4a317b310f3b4f6013fa59e43c3
Leandro Castillo Borja <leccbo1995@gmail.com>
2023-01-06 15:40:10 -0500

    Maquetar lista de movimientos de productos en recyclerView

--- app/src/main/java/com/leandro1995/retoandroid/activity/DetailProductActivity.kt
+++ app/src/main/java/com/leandro1995/retoandroid/activity/DetailProductActivity.kt
@@ -2,12 +2,28 @@ package com.leandro1995.retoandroid.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.leandro1995.retoandroid.R
+import com.leandro1995.retoandroid.adapter.MovementProductAdapter
 
 class DetailProductActivity : AppCompatActivity() {
+
+    private lateinit var movementProductAdapter: MovementProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_product)
+
+        movementProductAdapter = MovementProductAdapter()
+
+        findViewById<RecyclerView>(R.id.movementProductRecycler).apply {
+
+            layoutManager = LinearLayoutManager(this@DetailProductActivity).apply {
+                orientation = LinearLayoutManager.VERTICAL
+            }
+
+            adapter = movementProductAdapter
+        }
     }
 }
